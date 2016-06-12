dd73a13fcadcc2adf0b070897390ea78fc7a5103
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-06-12 18:14:10 +0900

    first commit

---
+++ csv2json.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	r := csv.NewReader(os.Stdin)
+	header, err := r.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	record := make(map[string]string)
+	enc := json.NewEncoder(os.Stdout)
+
+	for {
+		row, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for i, name := range header {
+			record[name] = row[i]
+		}
+		if err := enc.Encode(&record); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
